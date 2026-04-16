5eef5b33ed253ad1ada79f39345ddb4f98b06e59
AppleMao <348662181@qq.com>
2026-04-16 21:09:17 +0800

    刚刚没改成，再改一遍

--- cppProject1.cpp
+++ cppProject1.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
+typedef struct {
+    int a;
+    int b;
+    int total1 = a + b;
+    int total2 = (a - b < 0) ? b - a : a - b;
+    int total3 = a * b;
+}Total;
 int main() 
 { 
-    cout << "Hello world" << endl; 
-    return 0; 
+    Total total[10];
+    for (int i = 0;i < 10;i++)
+    {
+        cin >> total[i].a >> total[i].b;
+    }
+    for (int i = 0;i < 10;i++)
+    {
+        cout << total[i].total1 << " " << total[i].total2 << " " << total[i].total3 << endl;
+    }
+    return 0;
 }
